fd6e55b24ba4015babc7b91bba70b5afe697dbeb
lilyg <lilyg@LAPTOP-4G8H0187>
2023-04-29 00:31:17 -0500

    Add project files.

---
+++ spin the wheel/Form1.Designer.vb
@@ -0,0 +1,71 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.picWheel = New System.Windows.Forms.PictureBox()
+        Me.btnSpin = New System.Windows.Forms.Button()
+        Me.TimerSpin = New System.Windows.Forms.Timer(Me.components)
+        CType(Me.picWheel, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'picWheel
+        '
+        Me.picWheel.Image = Global.spin_the_wheel.My.Resources.Resources.spin_0
+        Me.picWheel.Location = New System.Drawing.Point(104, 32)
+        Me.picWheel.Name = "picWheel"
+        Me.picWheel.Size = New System.Drawing.Size(561, 368)
+        Me.picWheel.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.picWheel.TabIndex = 0
+        Me.picWheel.TabStop = False
+        '
+        'btnSpin
+        '
+        Me.btnSpin.Location = New System.Drawing.Point(339, 406)
+        Me.btnSpin.Name = "btnSpin"
+        Me.btnSpin.Size = New System.Drawing.Size(94, 41)
+        Me.btnSpin.TabIndex = 1
+        Me.btnSpin.Text = "Spin"
+        Me.btnSpin.UseVisualStyleBackColor = True
+        '
+        'TimerSpin
+        '
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 479)
+        Me.Controls.Add(Me.btnSpin)
+        Me.Controls.Add(Me.picWheel)
+        Me.Name = "frmMain"
+        Me.Text = "Spin the Wheel!"
+        CType(Me.picWheel, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents picWheel As PictureBox
+    Friend WithEvents btnSpin As Button
+    Friend WithEvents TimerSpin As Timer
+End Class
---
+++ spin the wheel/Form1.vb
@@ -0,0 +1,69 @@
+﻿Public Class frmMain
+    Dim Number As Integer
+    Dim Random As Decimal
+    Dim Number2 As Integer
+    Private Sub btnSpin_Click(sender As Object, e As EventArgs) Handles btnSpin.Click
+        TimerSpin.Enabled = True
+
+    End Sub
+
+    Private Sub TimerSpin_Tick(sender As Object, e As EventArgs) Handles TimerSpin.Tick
+        Number += 1
+        Number2 += 1
+
+        If Number = 1 Then
+            picWheel.Image = My.Resources.spin_0
+        ElseIf Number = 2 Then
+            picWheel.Image = My.Resources.spin_1
+        ElseIf Number = 3 Then
+            picWheel.Image = My.Resources.spin_2
+        ElseIf Number = 4 Then
+            picWheel.Image = My.Resources.spin_3
+        ElseIf Number = 5 Then
+            picWheel.Image = My.Resources.spin_4
+        ElseIf Number = 6 Then
+            picWheel.Image = My.Resources.spin_5
+        ElseIf Number = 7 Then
+            picWheel.Image = My.Resources.spin_6
+        ElseIf Number = 8 Then
+            picWheel.Image = My.Resources.spin_7
+        ElseIf Number = 9 Then
+            picWheel.Image = My.Resources.spin_8
+        ElseIf Number = 10 Then
+            picWheel.Image = My.Resources.spin_9
+        ElseIf Number = 11 Then
+            picWheel.Image = My.Resources.spin_10
+        ElseIf Number = 12 Then
+            picWheel.Image = My.Resources.spin_11
+        ElseIf Number = 13 Then
+            picWheel.Image = My.Resources.spin_12
+        ElseIf Number = 14 Then
+            picWheel.Image = My.Resources.spin_13
+        ElseIf Number = 15 Then
+            picWheel.Image = My.Resources.spin_14
+        ElseIf Number = 16 Then
+            picWheel.Image = My.Resources.spin_15
+        ElseIf Number = 17 Then
+            picWheel.Image = My.Resources.spin_16
+        ElseIf Number = 18 Then
+            picWheel.Image = My.Resources.spin_17
+        ElseIf Number = 19 Then
+            picWheel.Image = My.Resources.spin_18
+        ElseIf Number = 20 Then
+            picWheel.Image = My.Resources.spin_19
+        ElseIf Number = 21 Then
+            picWheel.Image = My.Resources.spin_20
+        ElseIf Number = 22 Then
+            picWheel.Image = My.Resources.spin_21
+        ElseIf Number = 23 Then
+            picWheel.Image = My.Resources.spin_22
+        ElseIf Number = 24 Then
+            picWheel.Image = My.Resources.spin_23
+        ElseIf Number = 25 Then
+            picWheel.Image = My.Resources.spin_24
+        ElseIf Number = 26 Then
+            picWheel.Image = My.Resources.spin_25
+
+        End If
+    End Sub
+End Class
